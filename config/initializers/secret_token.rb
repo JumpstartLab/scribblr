4536593339b0af185229e2f0589f36a7a95c88e1
Nick Gauthier <ngauthier@gmail.com>
2013-06-01 15:12:43 -0400

    initial application with posts scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scribblr::Application.config.secret_key_base = 'bcd3fbeea8fd9a268a9c80ff0c3d587774a5c7180cf2505d5950e91710ceae0ba8dece7adfad2cbbed0e7c8bca3863e5b27dae3925a38110df6c6ca85a65393b'
