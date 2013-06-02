d1cd604aa72bffccef7ab8adac32d67d8856cad8
Nick Gauthier <ngauthier@gmail.com>
2013-06-02 12:12:43 -0400

    autosaving

--- app/assets/javascripts/autosave.js
+++ app/assets/javascripts/autosave.js
@@ -1,26 +1,57 @@
 $(function() {
-  $("form[data-autosave]").on("submit", function(event) {
-    event.preventDefault();
+  var $form = $("form[data-autosave]");
+  var savedForm = $form.serialize();
+  var autosaveInterval = window.setInterval(saveFormIfChanged, 10000);
+
+  $(window).on("unload", function() {
+    saveFormIfChanged(false);
+  });
+
+  $form.on("submit", function() {
+    savedForm = $(this).serialize();
+  });
+
+  function notify(message, level) {
+    if (level === undefined) level = "info";
+    // display a notification under the page header
+    $('<div class="alert alert-' + level + '">' + message + '</div>').
+      appendTo('.page-header').
+      delay(3000).
+      fadeOut(500, function() {
+        $(this).remove()
+      });
+  }
+
+  function saveForm(async) {
+    if (async === undefined) async = true;
+
     $.ajax({
-      url: $(this).attr("action"),
+      url: $form.attr("action"),
       type: "POST",
-      data: $(this).serialize(),
-      dataType: "json"
+      data: $form.serialize(),
+      dataType: "json",
+      async: async
     }).done(function(data, status, response) {
       notify("Post saved.", "success");
-    }).fail(function(data, status, response) {
-      notify("Post failed to save.", "error");
-    });
-  });
+    }).fail(formError);
+  }
+
+  function saveFormIfChanged(async) {
+    var currentForm = $form.serialize();
+
+    if (currentForm !== savedForm) {
+      saveForm(async);
+      savedForm = currentForm;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function formError() {
+    clearInterval(autosaveInterval);
+    $(window).off("unload");
+    $form.off("submit");
+  }
 });
 
-function notify(message, level) {
-  if (level === undefined) level = "info";
-  // display a notification under the page header
-  $('<div class="alert alert-' + level + '">' + message + '</div>').
-    appendTo('.page-header').
-    delay(3000).
-    fadeOut(500, function() {
-      $(this).remove()
-    });
-}
